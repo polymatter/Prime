9b01821a8500d7d8cb35e4d69e4066e72b23ade3
Jeffrey D.S. Lake <glisher_rock@hotmail.com>
2011-12-07 12:10:30 +0000

    blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prime::Application.config.secret_token = 'e9b2695bddf08185e60de5351bce121544b2a3d07386f2b5847d7b14135ae61afed637665e60a153b309c3c74ca5cc28075655df4923dbc168d5076964bb8413'
