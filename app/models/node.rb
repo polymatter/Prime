8a87f49ae0042c833e4715ddc50c0c95bd895c64
Jeffrey Lake <glisher_rock@hotmail.com>
2011-12-11 21:25:50 +0000

    Show links to other nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,7 @@
 class Node < ActiveRecord::Base
   has_many :units
   
-  # define valid links
+  # define links to other nodes
   has_many :node_links
   has_many :linked_nodes, :through => :node_links
   
