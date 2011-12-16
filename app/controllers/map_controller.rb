c1a28b1a620aecbef2bc863f370243b0bc348a2c
Jeffrey Lake <glisher_rock@hotmail.com>
2011-12-16 15:16:43 +0000

    show reachable nodes and current unit on map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   def show
 	@nodes = Node.find(:all)
 	@units = Unit.find(:all)
-	if current_player 
+	if current_player.present? 
 	  @current_unit = current_player.units.first
 	  @nodelinks = @current_unit.node.node_links
 	end
