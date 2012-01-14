2376f9fc92dee9ca3bc144644ecb40ba598c86d0
Jeffrey Lake <glisher_rock@hotmail.com>
2012-01-14 14:44:32 +0000

    Battle is used primarily for namespacing so refactored as a utility module

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,12 +1,12 @@
 class TurnsController < ApplicationController
 
   def index
-    Unit.resolve_move_and_battles
-	
-	respond_to do |format|
+    Battle.resolve_move_and_battles
+
+    respond_to do |format|
       format.html { redirect_to map_path, notice: 'Turn update complete' }
       format.json { head :ok }
-	end
+    end
   end
 
 end
--- app/models/battle.rb
+++ app/models/battle.rb
@@ -6,10 +6,10 @@ module Battle
   def self.resolve_move_and_battles
     # Move human units and computer units seperately to avoid units bypassing eachother
     Unit.select { |unit| unit.moving? && unit.human? }.each { |unit| unit.move }
-	Node.select { |node| node.under_attack? }.each { |node| resolve_battle_at(node) }
+    Node.select { |node| node.under_attack? }.each { |node| resolve_battle_at(node) }
   
     Unit.select { |unit| unit.moving? && !unit.human? }.each { |unit| unit.move }
-	Node.select { |node| node.under_attack? }.each { |node| resolve_battle_at(node) }
+    Node.select { |node| node.under_attack? }.each { |node| resolve_battle_at(node) }
   
     Unit.select { |unit| unit.moving? }.each { |unit| unit.stop }
   end
@@ -20,35 +20,33 @@ module Battle
     
     # attackers strength is the sum of all the strengths of the attacking units, in the appropriate colour
     attackers_strength = node.attackers.map {|unit| unit[battle_colour] }.inject(0,:+)
-	
+
     # defenders strength is the max of node strength and the sum of defender strength
     sum_of_defender_units_strength = node.defenders.map {|unit| unit[battle_colour]}.inject(0,:+)
     defenders_strength = [sum_of_defender_units_strength, node.strength].max
-   	
-	msg = "#{node.name}, in a #{battle_colour} battle: ATT #{attackers_strength} to DEF #{defenders_strength}"
-	# attackers win
-	if attackers_strength > defenders_strength
-	  winners = node.attackers ; losers = node.defenders
-	  win_msg = ->(unit){"#{unit.name} conquer #{msg}"} ; fail_msg = ->(unit){"#{unit.name} lost #{msg}"}
-	  node.take_damage
+   
+    msg = "#{node.name}, in a #{battle_colour} battle: ATT #{attackers_strength} to DEF #{defenders_strength}"
+    # attackers win
+    if attackers_strength > defenders_strength
+      winners = node.attackers ; losers = node.defenders
+      win_msg = ->(unit){"#{unit.name} conquer #{msg}"} ; fail_msg = ->(unit){"#{unit.name} lost #{msg}"}
+      node.take_damage
       node.change_allegiance
-	# defenders win
-	else
-	  winners = node.defenders ; losers = node.attackers
-	  win_msg = ->(unit){"#{unit.name} held #{msg}"} ; fail_msg = ->(unit){"#{unit.name} failed to capture #{msg}"}
-	  node.take_damage
-	end
-	
-	winners.each do |unit| 
-	  unit.log(@@win, win_msg.call(unit))
-      unit.stop
-	end
-	
-	losers.each do |unit| 
-	  unit.log(@@fail, fail_msg.call(unit))
-	  unit.retreat
+    # defenders win
+    else
+      winners = node.defenders ; losers = node.attackers
+      win_msg = ->(unit){"#{unit.name} held #{msg}"} ; fail_msg = ->(unit){"#{unit.name} failed to capture #{msg}"}
+      node.take_damage
     end
-	
-  end
  
+    winners.each do |unit| 
+      unit.log(@@win, win_msg.call(unit))
+      unit.stop
+    end
+    
+    losers.each do |unit| 
+      unit.log(@@fail, fail_msg.call(unit))
+      unit.retreat
+    end
+  end
 end
